1eb76efbaaaa41c582d5d5d6f5f94c956c2ee3bc
suryadi.hhb@gmail.com <suryadi.hhb@gmail.com>
2025-03-23 14:43:37 +0700

    create middleware.js

---
+++ middleware.js
@@ -0,0 +1,25 @@
+const { getBaseUrl } = require("./config/baseUrl");
+
+const setHeaders = (req, res, next) => {
+  const url = new URL(req.url, getBaseUrl(req));
+
+  // Set CORS Headers
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle Preflight (OPTIONS)
+  if (req.method === "OPTIONS") {
+    console.log(`Handling OPTIONS request for ${url.pathname}`);
+    res.writeHead(204);
+    return res.end();
+  }
+
+  next(); // next request handler
+};
+
+module.exports = { setHeaders };
