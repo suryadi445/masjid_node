2fb191f49f55a2b7a4f6288b4c4e4bcac0cd8d8e
suryadi.hhb@gmail.com <suryadi.hhb@gmail.com>
2025-03-23 14:41:52 +0700

    create config for db and base url

---
+++ config/baseUrl.js
@@ -0,0 +1,6 @@
+const getBaseUrl = (req) => {
+  const protocol = req.headers["x-forwarded-proto"] || req.protocol || "http";
+  return `${protocol}://${req.headers.host}`;
+};
+
+module.exports = { getBaseUrl };
