a8ef0f37afccc0b1f7f905b005f2f8dd8bce03c0
suryadi.hhb@gmail.com <suryadi.hhb@gmail.com>
2025-04-13 14:31:45 +0700

    add config for handle cookie in middleware

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const http = require("http");
 const { handleRequest } = require("./routes/api");
 const { setHeaders } = require("./middleware");
+const dotenv = require("dotenv");
 
-const PORT = 5000;
+dotenv.config();
+const PORT = process.env.APP_PORT;
+
+const cookieMiddleware = (req, res, next) => {
+  const cookies = req.headers.cookie ? parseCookies(req.headers.cookie) : {};
+  req.cookies = cookies;
+  next();
+};
+
+const parseCookies = (cookieHeader) => {
+  const cookies = {};
+  cookieHeader.split(";").forEach((cookie) => {
+    const parts = cookie.split("=");
+    cookies[parts[0].trim()] = parts[1] ? parts[1].trim() : "";
+  });
+  return cookies;
+};
 
 const server = http.createServer((req, res) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  setHeaders(req, res, () => {
-    handleRequest(req, res);
+  cookieMiddleware(req, res, () => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    setHeaders(req, res, () => {
+      handleRequest(req, res);
+    });
   });
 });
 
--- middleware.js
+++ middleware.js
@@ -5,7 +5,8 @@ const setHeaders = (req, res, next) => {
 
   // Set CORS Headers
   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
