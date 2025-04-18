2944f9b6f0067ecf79057013726ca7eb65e89716
suryadi.hhb@gmail.com <suryadi.hhb@gmail.com>
2025-04-18 14:10:57 +0700

    hanld error in app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const http = require("http");
-const cookieParser = require("cookie-parser");
 const { setHeaders, authMiddleware } = require("./middleware");
 const { handleRequest } = require("./routes/api");
 const dotenv = require("dotenv");
@@ -17,20 +16,29 @@ const parseCookies = (cookieHeader) => {
   const cookies = {};
   cookieHeader.split(";").forEach((cookie) => {
     const parts = cookie.split("=");
-    cookies[parts[0].trim()] = parts[1] ? parts[1].trim() : "";
+    cookies[parts[0].trim()] = parts[1]
+      ? decodeURIComponent(parts[1].trim())
+      : "";
   });
   return cookies;
 };
 
 const server = http.createServer((req, res) => {
-  cookieMiddleware(req, res, () => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    setHeaders(req, res, () => {
-      authMiddleware(req, res, () => {
-        handleRequest(req, res);
+  try {
+    cookieMiddleware(req, res, () => {
+      console.log(`Incoming request: ${req.method} ${req.url}`);
+      setHeaders(req, res, () => {
+        authMiddleware(req, res, () => {
+          handleRequest(req, res);
+        });
       });
     });
-  });
+  } catch (err) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "Internal Server Error", error: err.message })
+    );
+  }
 });
 
 server.listen(PORT, () => {
